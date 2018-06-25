6e3d83267ca6f26ab039b834d888b193e28a103a
sclem <clem.spencer@gmail.com>
2018-06-25 13:14:10 -0400

    Add coupons for search results

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 ### Harbor Freight Coupons Extension
 
+#### Get the extension here:
+
+https://chrome.google.com/webstore/detail/harbor-freight-coupons/hhbonmijejindhnfgcdggfdbklphhloj
+
 #### Demo:
 
 ![demo](https://user-images.githubusercontent.com/8977050/41861220-a0bf61cc-786e-11e8-9284-705c459b5f75.gif)
--- coupons.js
+++ coupons.js
@@ -18,65 +18,122 @@ function lookupCoupon(itemno, callback) {
     xhr.send();
 }
 
-var itemno = findItemNo();
+function handleSingleItemPage(priceboxdiv) {
+    var itemno = findItemNo();
 
-var priceboxdiv = document.body.querySelector('.price-box');
-var saleCSS = false;
+    var saleCSS = false;
+    if (priceboxdiv) {
+        var salebox = priceboxdiv.querySelector('.sale');
+        if (salebox) {
+            saleCSS = true;
+            priceboxdiv = salebox;
+        }
+    }
 
-if (priceboxdiv) {
-    var salebox = priceboxdiv.querySelector('.sale');
-    if (salebox) {
-        saleCSS = true;
-        priceboxdiv = salebox;
+    if (itemno) {
+        lookupCoupon(itemno, function(resp) {
+            var couponTitleText = document.createElement('span');
+            couponTitleText.style['display'] = 'inline-block';
+            couponTitleText.style['vertical-align'] = 'top';
+            couponTitleText.style['color'] = '#3a3a3a';
+            couponTitleText.style['vertical-align'] = 'top';
+            var margin = '5px 5px 0 10px';
+            var fontSize = '1.3em';
+            if (saleCSS) {
+                margin = '-3px 5px 0 5px';
+                fontSize = '0.6em';
+            }
+            couponTitleText.style['margin'] = margin;
+            couponTitleText.style['font-size'] = fontSize;
+            couponTitleText.title = 'Provided by hfqpdb.com';
+            couponTitleText.innerText = resp.error || '';
+            if (saleCSS) {
+                priceboxdiv.appendChild(couponTitleText);
+            } else {
+                priceboxdiv.insertBefore(couponTitleText, priceboxdiv.querySelector('.comp'));
+            }
+
+            if (resp.hasOwnProperty('bestPrice')) {
+                var couponLinkText = '$' + resp.bestPrice;
+                if (~(resp.bestPrice + '').toLowerCase().indexOf('free')) {
+                    couponLinkText = 'FREE';
+                }
+                couponTitleText.innerText = 'Best Coupon:';
+
+                var couponLink = document.createElement('a');
+                couponLink.href = resp.url;
+                couponLink.style['border'] = '2px dashed #308104';
+                couponLink.style['display'] = 'inline-block';
+                couponLink.style['color'] = '#308104';
+                couponLink.style['padding-left'] = '3px';
+                couponLink.style['padding-right'] = '3px';
+                couponLink.style['position'] = 'absolute';
+                couponLink.innerText = couponLinkText;
+                couponLink.title = 'Provided by hfqpdb.com';
+                if (saleCSS) {
+                    priceboxdiv.appendChild(couponLink);
+                } else {
+                    couponLink.style['font-size'] = '2.5em';
+                    priceboxdiv.insertBefore(couponLink, priceboxdiv.querySelector('.comp'));
+                }
+            }
+        });
     }
 }
 
-if (itemno) {
-    lookupCoupon(itemno, function(resp) {
-        var couponTitleText = document.createElement('span');
-        couponTitleText.style['display'] = 'inline-block';
-        couponTitleText.style['vertical-align'] = 'top';
-        couponTitleText.style['color'] = '#3a3a3a';
-        couponTitleText.style['vertical-align'] = 'top';
-        var margin = '5px 5px 0 10px';
-        var fontSize = '1.3em';
-        if (saleCSS) {
-            margin = '-3px 5px 0 5px';
-            fontSize = '0.6em';
-        }
-        couponTitleText.style['margin'] = margin;
-        couponTitleText.style['font-size'] = fontSize;
-        couponTitleText.title = 'Provided by hfqpdb.com';
-        couponTitleText.innerText = resp.error || '';
-        if (saleCSS) {
-            priceboxdiv.appendChild(couponTitleText);
-        } else {
-            priceboxdiv.insertBefore(couponTitleText, priceboxdiv.querySelector('.comp'));
-        }
-
-        if (resp.hasOwnProperty('bestPrice')) {
-            var couponLinkText = '$' + resp.bestPrice;
-            if (~(resp.bestPrice + '').toLowerCase().indexOf('free')) {
-                couponLinkText = 'FREE';
-            }
-            couponTitleText.innerText = 'Best Coupon:';
-
-            var couponLink = document.createElement('a');
-            couponLink.href = resp.url;
-            couponLink.style['border'] = '2px dashed #308104';
-            couponLink.style['display'] = 'inline-block';
-            couponLink.style['color'] = '#308104';
-            couponLink.style['padding-left'] = '3px';
-            couponLink.style['padding-right'] = '3px';
-            couponLink.style['position'] = 'absolute';
-            couponLink.innerText = couponLinkText;
-            couponLink.title = 'Provided by hfqpdb.com';
-            if (saleCSS) {
-                priceboxdiv.appendChild(couponLink);
-            } else {
-                couponLink.style['font-size'] = '2.5em';
-                priceboxdiv.insertBefore(couponLink, priceboxdiv.querySelector('.comp'));
-            }
-        }
-    });
+function findListItemNumber(priceboxdiv) {
+    var itemno = 0;
+    try {
+        itemno = priceboxdiv.parentNode.querySelector('.product-ids').innerText.toLowerCase().split('item #')[1].trim();
+    } catch (ex) {}
+    return itemno;
 }
+
+function displayCoupons() {
+    var priceboxdivs = document.body.querySelectorAll('.price-box');
+
+    if (!priceboxdivs) {
+        return;
+    }
+
+    if (priceboxdivs.length === 1) {
+        handleSingleItemPage(priceboxdivs[0]);
+        return;
+    } else {
+        priceboxdivs.forEach(function(item) {
+            var itemno = findListItemNumber(item);
+
+            if (itemno) {
+                lookupCoupon(itemno, function(resp) {
+                    if (resp.hasOwnProperty('bestPrice')) {
+                        var couponLink = document.createElement('a');
+                        var couponLinkText = '$' + resp.bestPrice;
+                        if (~(resp.bestPrice + '').toLowerCase().indexOf('free')) {
+                            couponLinkText = 'FREE';
+                        }
+                        couponLink.href = resp.url;
+                        couponLink.innerText = couponLinkText;
+
+                        couponLink.style['display'] = 'inline-block';
+                        couponLink.style['border'] = '2px dashed #308104';
+                        couponLink.style['color'] = '#308104';
+                        couponLink.style['padding'] = '2px';
+                        couponLink.style['float'] = 'right';
+                        couponLink.style['margin-top'] = '5px';
+                        couponLink.style['margin-right'] = '2px';
+                        couponLink.style['font-size'] = '1.3em';
+                        couponLink.title = 'Provided by hfqpdb.com';
+
+                        var insertNode = item.querySelector('.clear');
+                        if (!insertNode) {
+                            insertNode = item.querySelector('.comp');
+                        }
+                        item.insertBefore(couponLink, insertNode);
+                    }
+                });
+            }
+        });
+    }
+}
+
+displayCoupons();
